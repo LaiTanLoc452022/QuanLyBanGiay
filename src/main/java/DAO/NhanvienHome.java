c74401264d5a95f505f516ea583c6f00a8a3b304
LaiTanLoc452022 <103021684+LaiTanLoc452022@users.noreply.github.com>
2023-05-19 10:06:08 +0700

    commit

---
+++ src/main/java/DAO/NhanvienHome.java
@@ -0,0 +1,20 @@
+package DAO;
+// Generated Mar 30, 2023, 7:14:19 PM by Hibernate Tools 5.6.12.Final
+import entity1.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Nhanvien.
+ * @see DAO.Nhanvien
+ * @author Hibernate Tools
+ */
+public class NhanvienHome extends Generic_Implement {
+
+
+}
